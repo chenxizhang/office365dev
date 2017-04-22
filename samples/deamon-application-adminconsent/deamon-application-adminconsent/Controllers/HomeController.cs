6b31fddc4656fb45ee659c12894d3e24a34e205d
Ares Chen <arechen@microsoft.com>
2017-04-22 16:34:13 +0800

    注册 Azure AD应用程序

---
+++ samples/deamon-application-adminconsent/deamon-application-adminconsent/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace deamon_application_adminconsent.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            /*
+             * https://login.windows.net/59723f6b-2d14-49fe-827a-8d04f9fe7a68/oauth2/token
+             * a38a723a-23c9-40e8-b4cb-23f3f337b030
+             * ju9iG9RuRxdkeFkXVQ2S1EgBW2k5UUA/l3fQfpWft1E=
+             */
+            //https://login.microsoftonline.com/common/adminconsent?client_id=a38a723a-23c9-40e8-b4cb-23f3f337b030&state=12345&redirect_uri=http://graphadminconsent.azurewebsites.net/
+            return View();
+        }
+    }
+}
---
+++ samples/deamon-application-adminconsent/deamon-application-adminconsent/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="deamon_application_adminconsent.MvcApplication" Language="C#" %>
