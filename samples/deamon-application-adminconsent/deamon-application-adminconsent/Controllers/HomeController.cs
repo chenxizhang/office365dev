0a316e78f4f16c41b86cbb3bd8cf483188973f42
Ares Chen <arechen@microsoft.com>
2017-05-02 21:30:32 +0800

    创建了web application sample

--- samples/deamon-application-adminconsent/deamon-application-adminconsent/Controllers/HomeController.cs
+++ samples/deamon-application-adminconsent/deamon-application-adminconsent/Controllers/HomeController.cs
@@ -9,6 +9,10 @@ namespace deamon_application_adminconsent.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
 
@@ -17,8 +21,27 @@ namespace deamon_application_adminconsent.Controllers
              * a38a723a-23c9-40e8-b4cb-23f3f337b030
              * ju9iG9RuRxdkeFkXVQ2S1EgBW2k5UUA/l3fQfpWft1E=
              */
-            //https://login.microsoftonline.com/common/adminconsent?client_id=a38a723a-23c9-40e8-b4cb-23f3f337b030&state=12345&redirect_uri=http://graphadminconsent.azurewebsites.net/
+            //https://login.microsoftonline.com/59723f6b-2d14-49fe-827a-8d04f9fe7a68/adminconsent?client_id=09082ce3-13c3-4d15-9799-431316d8b728&state=12345&redirect_uri=https://graphadminconsent.azurewebsites.net/
+
+
+
+            /*
+            POST https://login.microsoftonline.com/59723f6b-2d14-49fe-827a-8d04f9fe7a68/oauth2/token HTTP/1.1
+            Content - Type: application / x - www - form - urlencoded
+
+            grant_type = client_credentials
+            & client_id =09082ce3-13c3-4d15-9799-431316d8b728
+            &client_secret =+USDZqA76m2EFBSj2T/IaHNosf7adJSLEtVUoE5/MHM=
+            &resource = https://graph.microsoft.com 
+            */
+
+            /*
+            GET https://graph.microsoft.com/v1.0/users
+            Authorization: Bearer <token>
+            */
+
             return View();
         }
     }
 }
+ 
---
+++ samples/graph-webapplicationsample/graph-webapplicationsample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="graph_webapplicationsample.MvcApplication" Language="C#" %>
