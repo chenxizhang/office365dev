4cbf7f3529fa5616c4a11754d29287308c4740ed
Ares Chen <arechen@microsoft.com>
2017-05-26 08:40:28 +0800

    增加对gallatin的支持

---
+++ samples/Office365GraphMVCHelper/restorepackage.ps1
@@ -0,0 +1,7 @@
+﻿[xml]$xml = Get-Content .\packages.xml
+$nodes = $xml.packages
+
+foreach($item in $nodes.package){
+	#Uninstall-Package $item.id
+	Install-Package $item.id -Version $item.version
+}
