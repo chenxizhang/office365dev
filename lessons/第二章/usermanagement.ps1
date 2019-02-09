d8710123639e4147a0301bd2a5e93b9c538b2728
Ares Chen <arechen@microsoft.com>
2019-02-09 11:03:41 +0800

    用户管理的脚本

---
+++ lessons/第二章/usermanagement.ps1
@@ -0,0 +1,44 @@
+# 这个脚本演示如何通过PowerShell管理Office 365用户
+# 作者：陈希章 《Office 365 开发入门指南》第二版  于 2019-2-9
+
+# 准备工作，请通过单独打开一个Powershell窗口，并用管理员方式打开
+# Install-Module -Name AzureAD
+
+# 第一步，连接到国际版的Office 365
+Connect-MsolService 
+
+# 连接到国内版本的Office 365
+Connect-MsolService -AzureEnvironment AzureChinaCloud
+
+# 获取所有的用户列表
+Get-MsolUser
+
+# 获取还没有授权的用户列表
+Get-MsolUser -UnlicensedUsersOnly
+
+# 创建新用户
+New-MsolUser
+
+# 修改用户
+Set-MsolUser
+
+# 删除用户
+Remove-MsolUser
+
+# 创建组
+New-MsolGroup
+
+# 修改组
+Set-MsolGroup
+
+# 删除组
+Remove-MsolGroup
+
+# 添加组成员
+Add-MsolGroupMember
+
+# 获取组成员
+Get-MsolGroupMember
+
+# 移除组成员
+Remove-MsolGroupMember
