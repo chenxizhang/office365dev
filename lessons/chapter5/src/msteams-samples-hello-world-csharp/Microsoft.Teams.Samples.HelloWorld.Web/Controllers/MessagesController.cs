80fee117c0d9c5b17755a085c0ea0770f9c751f6
Ares Chen <arechen@microsoft.com>
2019-03-03 18:34:16 +0800

    修订代码

--- lessons/chapter5/src/msteams-samples-hello-world-csharp/Microsoft.Teams.Samples.HelloWorld.Web/Controllers/MessagesController.cs
+++ lessons/chapter5/src/msteams-samples-hello-world-csharp/Microsoft.Teams.Samples.HelloWorld.Web/Controllers/MessagesController.cs
@@ -30,15 +30,9 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Controllers
                 {
                     if(activity.Type == "message")
                     {
-                        var text = activity.GetTextWithoutMentions();
-                        if (text == "抽奖")
-                        {
-                            await Conversation.SendAsync(activity, () => new ProactiveMsgTo1to1Dialog());
-                        }
-                        else
-                        {
+
                             await EchoBot.EchoMessage(connector, activity);
-                        }
+      
                     }
 
                     return new HttpResponseMessage(HttpStatusCode.Accepted);
@@ -48,49 +42,4 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Controllers
     }
 
 
-    /// <summary>
-    /// This is Proactive Message Dialog Class. Main purpose of this class is to show the Send Proactive Message Example
-    /// </summary>
-    [Serializable]
-    public class ProactiveMsgTo1to1Dialog : IDialog<object>
-    {
-        public async Task StartAsync(IDialogContext context)
-        {
-            if (context == null)
-            {
-                throw new ArgumentNullException(nameof(context));
-            }
-
-            //Set the Last Dialog in Conversation Data
-            context.UserData.SetValue("抽奖", "抽奖数据");
-
-            var userId = context.Activity.From.Id;
-            var botId = context.Activity.Recipient.Id;
-            var botName = context.Activity.Recipient.Name;
-
-            var channelData = context.Activity.GetChannelData<TeamsChannelData>();
-            var connectorClient = new ConnectorClient(new Uri(context.Activity.ServiceUrl));
-
-            var parameters = new ConversationParameters
-            {
-                Bot = new ChannelAccount(botId, botName),
-                Members = new ChannelAccount[] { new ChannelAccount(userId) },
-                ChannelData = new TeamsChannelData
-                {
-                    Tenant = channelData.Tenant
-                }
-            };
-
-            var conversationResource = await connectorClient.Conversations.CreateConversationAsync(parameters);
-
-            var message = Activity.CreateMessageActivity();
-            message.From = new ChannelAccount(botId, botName);
-            message.Conversation = new ConversationAccount(id: conversationResource.Id.ToString());
-            message.Text ="抽奖消息";
-
-            await connectorClient.Conversations.SendToConversationAsync((Activity)message);
-
-            context.Done<object>(null);
-        }
     }
-}
