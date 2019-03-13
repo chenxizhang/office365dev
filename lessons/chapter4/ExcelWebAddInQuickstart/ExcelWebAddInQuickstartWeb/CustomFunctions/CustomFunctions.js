9a77313b4462768fde39d7aece20652afcf2ae3f
Ares Chen <arechen@microsoft.com>
2019-03-13 21:53:09 +0800

    custom functions

---
+++ lessons/chapter4/ExcelWebAddInQuickstart/ExcelWebAddInQuickstartWeb/CustomFunctions/CustomFunctions.js
@@ -0,0 +1,5 @@
+﻿function add(first, second) {
+    return first + second;
+}
+// associate `id` values in the JSON metadata file to the JavaScript function names
+CustomFunctions.associate("ADD", add);
